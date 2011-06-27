276ceeef4f0e260cedbc1aa0f794d27721a95780
lehui99 <lehui99@667d7818-9922-ae22-6c3c-e2208f24cb88>
2011-06-27 07:15:36 +0000

    fg dns test

--- DNSTest/cpp/fgdns/fgdns/fgdns.cpp
+++ DNSTest/cpp/fgdns/fgdns/fgdns.cpp
@@ -24,23 +24,34 @@ unsigned short cksum(unsigned char* buf, int len)
 	return ~sum;
 }
 
-void genPktLnHead(unsigned char* buf, const unsigned char* srcMac, const unsigned char* destMac)
+int genPktLnHead(unsigned char* buf, const unsigned char* srcMac, const unsigned char* destMac, const char* subProto)
 {
 	memcpy(buf, destMac, 6);
 	memcpy(buf + 6, srcMac, 6);
-	memcpy(buf + 12, "\x08", 2);
+	memcpy(buf + 12, subProto, 2);
+	return 14;
 }
 
-void genPktIpHead(unsigned char* buf, const unsigned char* srcIp, const unsigned char* destIp)
+int genPktIpHead(unsigned char* buf, const unsigned char* srcIp, const unsigned char* destIp)
 {
-	memcpy(buf + 14, "\x45\x00\x00\x7b", 4);
+	memcpy(buf, "\x45\x00\x00\x7b", 4);
 	unsigned short id = rand() + rand();
-	memcpy(buf + 18, &id, 2);
-	memcpy(buf + 20, "\x00\x00\x80\x11\x00", 6);
-	memcpy(buf + 26, srcIp, 4);
-	memcpy(buf + 30, destIp, 4);
-	unsigned short sum = cksum(buf + 14, 30 + 4 - 14);
-	memcpy(buf + 24, &sum, 2);
+	memcpy(buf + 4, &id, 2);
+	memcpy(buf + 6, "\x00\x00\x80\x11\x00", 6);
+	memcpy(buf + 12, srcIp, 4);
+	memcpy(buf + 16, destIp, 4);
+	unsigned short sum = cksum(buf, 20);
+	memcpy(buf + 10, &sum, 2);
+	return 20;
+}
+
+int genPktPPPoEHead(unsigned char* buf, const unsigned char* sessionId, const char* subProto)
+{
+	memcpy(buf, "\x11", 2);
+	memcpy(buf + 2, sessionId, 2);
+	memcpy(buf + 4, "\x00\x7d", 2);
+	memcpy(buf + 6, subProto, 2);
+	return 8;
 }
 
 unsigned char randChr()
@@ -49,32 +60,51 @@ unsigned char randChr()
 	return chrs[rand() % (sizeof(chrs) - 1)];
 }
 
-void genPktUdpHead(unsigned char* buf)
+int genPktUdpHead(unsigned char* buf)
 {
 	unsigned short srcPort = rand() + rand();
-	memcpy(buf + 34, &srcPort, 2);
-	memcpy(buf + 36, "\x00\x35\x00\x67\x00", 6);
+	memcpy(buf, &srcPort, 2);
+	memcpy(buf + 2, "\x00\x35\x00\x67\x00", 6);
 
 	//domain
 	unsigned short transId =rand() + rand();
-	memcpy(buf + 42, &transId, 2);
-	memcpy(buf + 44, "\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x1e", 11);
+	memcpy(buf + 8, &transId, 2);
+	memcpy(buf + 10, "\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x1e", 11);
 	for(int i = 0; i < 30; i++)
-		buf[55 + i] = randChr();
-	buf[85] = (char) 31;
+		buf[21 + i] = randChr();
+	buf[51] = (char) 31;
 	for(int i = 0; i < 31; i++)
-		buf[86 + i] = randChr();
-	memcpy(buf + 117, "\x02\x37\x31\x07nsk3fxb\x03\x63om\x00\x00\x0f\x00\x01\x00\x00\x11\x00\x67", 25);
-	unsigned short sum = cksum(buf + 26, 117 + 25 - 26);
-	memcpy(buf + 40, &sum, 2);
+		buf[52 + i] = randChr();
+	memcpy(buf + 83, "\x02\x37\x31\x07nsk3fxb\x03\x63om\x00\x00\x0f\x00\x01\x00\x00\x11\x00\x67", 25);
+	unsigned short sum = cksum(buf - 8, 116);
+	memcpy(buf + 6, &sum, 2);
+	return 103;
 }
 
 int genPkt(unsigned char* buf, const unsigned char* srcMac, const unsigned char* destMac, const unsigned char* srcIp, const unsigned char* destIp)
 {
-	genPktLnHead(buf, srcMac, destMac);
-	genPktIpHead(buf, srcIp, destIp);
-	genPktUdpHead(buf);
-	return 137;
+	int offset = 0;
+	int len = genPktLnHead(buf + offset, srcMac, destMac, "\x08");
+	offset += len;
+	len = genPktIpHead(buf + offset, srcIp, destIp);
+	offset += len;
+	len = genPktUdpHead(buf + offset);
+	offset += len;
+	return offset;
+}
+
+int genPPPoEPkt(unsigned char* buf, const unsigned char* srcMac, const unsigned char* destMac, const unsigned char* sessionId, const unsigned char* srcIp, const unsigned char* destIp)
+{
+	int offset = 0;
+	int len = genPktLnHead(buf + offset, srcMac, destMac, "\x88\x64");
+	offset += len;
+	len = genPktPPPoEHead(buf + offset, sessionId, "\x00\x21");
+	offset += len;
+	len = genPktIpHead(buf + offset, srcIp, destIp);
+	offset += len;
+	len = genPktUdpHead(buf + offset);
+	offset += len;
+	return offset;
 }
 
 void genRndDestIp(unsigned char* destIp)
@@ -101,13 +131,47 @@ void textMac2BinMac(const char* textMac, unsigned char* binMac)
 		binMac[i] = (hex2bin(textMac[i * 3]) << 4) | hex2bin(textMac[i * 3 + 1]);
 }
 
+bool procPkt(const unsigned char* buf, unsigned char* srcMac, unsigned char* destMac, unsigned char* srcIp, const unsigned char* destIp, unsigned char* sessionId, bool& pppoe)
+{
+	if(memcmp(buf + 12, "\x08", 2) == 0)	//Ip
+	{
+		if(memcmp(buf + 30, destIp, 4) == 0)
+		{
+			pppoe = false;
+			memcpy(destMac, buf, 6);
+			memcpy(srcMac, buf + 6, 6);
+			memcpy(srcIp, buf + 26, 4);
+			return true;
+		}
+		return false;
+	}
+	else if(memcmp(buf + 12, "\x88\x64", 2) == 0)	//PPPoE
+	{
+		if(memcmp(buf + 19, "\x00\x21", 2) == 0)	//Ip
+		{
+			if(memcmp(buf + 38, destIp, 4) == 0)
+			{
+				pppoe = true;
+				memcpy(destMac, buf, 6);
+				memcpy(srcMac, buf + 6, 6);
+				memcpy(srcIp, buf + 34, 4);
+				memcpy(sessionId, buf + 16, 2);
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+	return false;
+}
+
 int main(int argc, char* argv[])
 {
 	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_if_t *alldevs;
 	if(pcap_findalldevs(&alldevs, errbuf) != 0)
 	{
-		cout << "pcap_findalldevs error: " << errbuf << endl;
+		cerr << "pcap_findalldevs error: " << errbuf << endl;
 		return 1;
 	}
 	pcap_t *dev;
@@ -130,7 +194,7 @@ int main(int argc, char* argv[])
 		dev = pcap_open_live(pcap_dev->name, 65535, 0, 0, errbuf);
 		if(dev == NULL)
 		{
-			cout << "pcap_open_live error: " << errbuf << endl;
+			cerr << "pcap_open_live error: " << errbuf << endl;
 			return 3;
 		}
 	}
@@ -151,9 +215,14 @@ int main(int argc, char* argv[])
 	}
 	else if(argv[1][0] == 'p')
 	{
+		unsigned char sessionId[2];
+		*(unsigned short*)sessionId = atoi(argv[5]);
 		while(true)
 		{
-			//TODO:
+			genRndDestIp(binDestIp);
+			int len = genPPPoEPkt(buf, binSrcMac, binDestMac, sessionId, binSrcIp, binDestIp);
+			pcap_sendpacket(dev, buf, len);
+			Sleep(20);
 		}
 	}
 	return 0;
